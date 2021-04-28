380a2c9791f70edab99cc982f64da0a44b1aa19d
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-04-28 06:38:04 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+const main = document.querySelector('main')
+main.remove()
+
+const newHeader = document.querySelector('h1')
+newHeader.innerHTML="<h1 id='victory'>Carlos is the champion</h1>"
